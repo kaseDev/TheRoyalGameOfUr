42036222785aa0069e83158ee8ff4c8570cc5c62
Brendan Sakosits <brendansakosits@gmail.com>
2021-07-16 19:13:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 from flask import Flask, Response, render_template, request
-print('hello world')
+
+app = Flask(__name__, template_folder='Templates')
+
+@app.route('/')
+def main():
+	return render_template('frontend.html')
+@app.route('/input', methods=['GET', 'POST'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__== '__main__':
+    app.run()
